6cb6e8f9b18d9d0431f497c42ef1cc395ddddc67
Lsnyy <119632440+Lsnyy@users.noreply.github.com>
2025-04-07 01:15:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ function loadGames(gamesToLoad) {
     gameList.innerHTML = "";
     gamesToLoad.forEach(game => {
         gameList.innerHTML += `
-            <a href="${game.url}" class="game-card" target="_blank">
-                <img src="${game.image}" alt="${game.title}">
-                <h3>${game.title}</h3>
-                <span class="category">${game.category}</span>
-            </a >
-        `;
+    <a href="${game.url}" class="game-card" target="_blank" rel="noreferrer">
+        < img src="${game.image}" alt="${game.title}">
+        <h3>${game.title}</h3>
+        <span class="category">${game.category}</span>
+    </a >
+`;
     });
 }
 
