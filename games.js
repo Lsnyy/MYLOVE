cae096b19890757371d9f38184a6e78c80a8e797
Lsnyy <119632440+Lsnyy@users.noreply.github.com>
2025-04-14 16:24:11 +0800

    Update games.js

--- games.js
+++ games.js
@@ -3,49 +3,49 @@ const games = [
         title: "所有资源均来源于网上大佬分享，完全免费",
         url: "#",
         category: "免费",
-        image: "images/MIANFEI.jpg"
+        image: "images/免费.jpg"
     },
     {
         title: "异星工厂",
         url: "https://pan.quark.cn/s/ba6797bbc52c",
         category: "建设",
-        image: "images/YIXING.jpg"
+        image: "images/异星工厂.jpg"
     },
     {
         title: "小骨：英雄杀手",
         url: "https://pan.quark.cn/s/b18ab9f6695c",
         category: "肉鸽",
-        image: "images/XIAOGU.jpg" // 确保文件名无冒号
+        image: "images/小骨英雄杀手.jpg" // 确保文件名无冒号
     },
     {
         title: "夏日狂想曲",
         url: "https://pan.quark.cn/s/eba1a7e159fb",
         category: "模拟",
-        image: "images/XIARI.jpg"
+        image: "images/夏日.jpg"
     },
     {
         title: "奥日",
         url: "https://pan.quark.cn/s/11964568e719",
         category: "动作",
-        image: "images/AORI.jpg" // 检查扩展名大小写
+        image: "images/奥日.jpg" // 检查扩展名大小写
     },
     {
         title: "沙威玛",
         url: "https://pan.quark.cn/s/80fc099652bd",
         category: "模拟",
-        image: "images/SHAWEI.jpg"
+        image: "images/沙威玛.jpg"
     },
     {
         title: "冬日狂想曲",
         url: "https://pan.quark.cn/s/f2afd2450c0f",
         category: "模拟",
-        image: "images/DONGRI.jpg"
+        image: "images/冬日.jpg"
     },
     {
         title: "夏色四叶草",
         url: "https://pan.quark.cn/s/cb210eedd845",
         category: "模拟",
-        image: "images/XIASE.jpg"
+        image: "images/夏色四叶草.jpg"
     },
     {
         title: "CE",
@@ -57,7 +57,7 @@ const games = [
         title: "大侠立志传",
         url: "https://pan.quark.cn/s/366d77556fbf",
         category: "模拟",
-        image: "images/DAXIA.jpg"
+        image: "images/大侠立志传.jpg"
     },
     {
         title: "LX Music",
@@ -69,18 +69,18 @@ const games = [
         title: "苏丹的游戏",
         url: "https://pan.quark.cn/s/1d1e9d11c6d1",
         category: "卡牌",
-        image: "images/SUDAN.jpg"
+        image: "images/苏丹的游戏.jpg"
     },
     {
         title: "小丑牌",
         url: "https://pan.quark.cn/s/60c7868f631d",
         category: "卡牌",
-        image: "images/XIAOCHOU.jpg"
+        image: "images/小丑牌.jpg"
     },
     {
         title: "饥荒",
         url: "https://pan.quark.cn/s/05b6ebe12941",
         category: "模拟",
-        image: "images/JIHUANG.jpg"
+        image: "images/饥荒.jpg"
     }
 ]; // ✅ 正确闭合数组
